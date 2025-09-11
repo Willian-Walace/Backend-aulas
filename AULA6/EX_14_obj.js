39c0cf2840db364b4c6671d1033971b7d27cbe7d
Willian-Walace <willianwalacemedeirosj@gmail.com>
2025-09-10 22:19:02 -0300

    AULA6
    exemplos de arrays e objetos

---
+++ AULA6/EX_14_obj.js
@@ -0,0 +1,13 @@
+// exemplo objeto
+
+let usuario ={
+    nome: 'Daniel',
+    idade: 30,
+    isAdmin: false
+}
+
+if(usuario.isAdmin===true){
+    console.log('Pode acessar o sistema');
+}else{
+    console.log('Usuário não é administrador');
+}
